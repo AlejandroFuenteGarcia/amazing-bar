14975ffd34b6ff11c3e65103a6803d54894aa026
Alejandro Fuente Garcia <afuengar2001@gmail.com>
2023-01-10 12:37:58 +0100

    Commit 5

--- dueño.java
+++ dueño.java
@@ -4,4 +4,10 @@ public class dueño
     private String nombreDuenio;
     private String dni;
     private bar barDelQueEsPropietario;
+
+    public dueño(String nombreDelDuenio, String dniDuenio, bar barDelPropietario){
+        nombreDuenio=nombreDelDuenio;
+        dni=dniDuenio;
+        barDelQueEsPropietario= barDelPropietario;
+    }
 }
