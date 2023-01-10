8ababaead872b12cf49a7576fb2af029422175df
Alejandro Fuente Garcia <afuengar2001@gmail.com>
2023-01-10 12:16:48 +0100

    Commit 1

---
+++ bar.java
@@ -0,0 +1,14 @@
+
+
+public class bar
+{
+    private String nombreBar;
+    private int aforo;
+
+    public bar(String nombreDelBar, int numeroDeAforo)
+    {
+        nombreBar= nombreDelBar;
+        aforo= numeroDeAforo;
+    }
+
+}
